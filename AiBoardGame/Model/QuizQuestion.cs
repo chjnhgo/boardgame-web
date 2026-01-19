0c789934657b7ab85fc6aa8b381fce34e6ee0eca
chjnhgo <hoanganhtran1209@gmail.com>
2026-01-19 15:19:14 +0700

    deploy lai

--- AiBoardGame/Model/QuizQuestion.cs
+++ AiBoardGame/Model/QuizQuestion.cs
@@ -1,11 +1,17 @@
-﻿namespace AiBoardGame.Models;
+﻿using System.Collections.Generic;
+
+namespace AiBoardGame.Models;
 
 public class QuizQuestion
 {
+    // --- THÊM DÒNG NÀY ĐỂ SỬA LỖI CS1061 ---
+    public int Id { get; set; }
+
     public string Content { get; set; } = "";
     public List<string> Options { get; set; } = new();
     public int CorrectIndex { get; set; }
     public int Difficulty { get; set; }
 
-    public QuizQuestion() { } // <--- DÒNG NÀY RẤT QUAN TRỌNG
+    // Constructor rỗng (Bắt buộc cho SignalR)
+    public QuizQuestion() { }
 }
